7ae0cede4ceae5e2cbe2956ec710de52947f5b1c
Pisani Cristian <pcristian.008@gmail.com>
2024-08-07 12:14:12 +0200

    Reajuste de firebaseConfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,10 +3,10 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.AUTH,
-  projectId: import.meta.env.PROJECT,
-  storageBucket: import.meta.env.STORAGE,
-  messagingSenderId: import.meta.env.MESSAGE,
+  authDomain: import.meta.env.VITE_AUTH,
+  projectId: import.meta.env.VITE_PROJECT,
+  storageBucket: import.meta.env.VITE_STORAGE,
+  messagingSenderId: import.meta.env.VITE_MESSAGE,
   appId: import.meta.env.APPID,
 };
 
