b79cfa4e8f0608a690295e62096e8dcba3127b3f
Cristian <126271844+pcristian08@users.noreply.github.com>
2024-08-09 01:28:04 +0200

    Add files via upload

--- src/pages/itemListContainer/ItemList.jsx
+++ src/pages/itemListContainer/ItemList.jsx
@@ -3,17 +3,19 @@ import "./itemList.css";
 
 export const ItemList = ({ items }) => {
   return (
-    <div className="cartas">
+    <div className="item-list-container">
       {items.map((elemento) => {
         return (
-          <Cartas
-            key={elemento.id} //Siempre se debe utilizar el valor "key" cuando utilizo el .map
-            title={elemento.title}
-            description={elemento.description}
-            price={elemento.price}
-            id={elemento.id}
-            img={elemento.img}
-          />
+          //Siempre se debe utilizar el valor "key" cuando utilizo el .map
+          <div className="item-card" key={elemento.id}>
+            <Cartas
+              title={elemento.title}
+              description={elemento.description}
+              price={elemento.price}
+              id={elemento.id}
+              img={elemento.img}
+            />
+          </div>
         );
       })}
     </div>
