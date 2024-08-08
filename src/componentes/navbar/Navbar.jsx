37be75b6a6e9aa43d8570187f9f15ac44f25aa1a
Cristian <126271844+pcristian08@users.noreply.github.com>
2024-08-09 01:25:21 +0200

    Add files via upload

--- src/componentes/navbar/Navbar.jsx
+++ src/componentes/navbar/Navbar.jsx
@@ -8,18 +8,29 @@ export const Navbar = () => {
       <>
         <div className="navcontainer">
           <Link to="/">
-            <img src="https://res.cloudinary.com/dubanhzqy/image/upload/v1719757811/pyp_hvehok.png"></img>
+            <img
+              className="logo"
+              src="https://res.cloudinary.com/dubanhzqy/image/upload/v1719757811/pyp_hvehok.png"
+            ></img>
           </Link>
           <ul>
-            <Link to="/">Todos</Link>
-            <Link to="/category/Impulsores">Impulsores</Link>
-            <Link to="/category/Reguladores">Reguladores de voltaje</Link>
-            <Link to="/category/Portadiodos">Porta diodos</Link>
-            <Link to="/category/Portaescobillas">Porta escobillas</Link>
-            <Link to="/category/Iluminacion">Iluminacion</Link>
-            <Link to="/category/Otros">Otros</Link>
+            <Link to="/category/Impulsores" className="my-link">
+              Impulsores
+            </Link>
+            <Link to="/category/Reguladores" className="my-link">
+              Reguladores de voltaje
+            </Link>
+            <Link to="/category/Portadiodos" className="my-link">
+              Porta diodos
+            </Link>
+            <Link to="/category/Portaescobillas" className="my-link">
+              Porta escobillas
+            </Link>
+            <Link to="/category/Iluminacion" className="my-link">
+              Iluminacion
+            </Link>
           </ul>
-          <CartWidget />
+          <CartWidget className="cart-widget" />
         </div>
       </>
     </div>
